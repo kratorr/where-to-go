9a75677b1022c580068e3f4a529f9222ec0ff60f
Vladimir <smirnovv91@gmail.com>
2020-10-25 18:11:27 +0300

    Add Place model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db.models import JSONField
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description_short = models.CharField(max_length=500, verbose_name='Короткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
+    coordinates = JSONField(verbose_name='Координаты')
+
+    def __str__(self):
+        return self.title
