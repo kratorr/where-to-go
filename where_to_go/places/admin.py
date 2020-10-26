e8fc4d333e84f1767564c88ea1bb8b3ca997f57c
kratorr <smirnovv91@gmail.com>
2020-10-26 09:25:27 +0300

    Add model for images

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from places.models import Place
+from places.models import Place, Image
 
 
 admin.site.register(Place)
+admin.site.register(Image)
--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
-from django.db.models import JSONField
-
 
 class Place(models.Model):
     title = models.CharField(max_length=50, verbose_name='Название')
     description_short = models.CharField(max_length=500, verbose_name='Короткое описание')
     description_long = models.TextField(verbose_name='Полное описание')
-    coordinates = JSONField(verbose_name='Координаты')
+    coordinates = models.JSONField(verbose_name='Координаты')
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
+    number = models.PositiveIntegerField(null=False)
+
+    def __str__(self):
+        return '{} {}'.format(self.number, self.place.title)
