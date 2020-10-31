6e688c01233caa7cfaf39c70e8f0e3ff9bfad4d1
kratorr <smirnovv91@gmail.com>
2020-10-31 11:37:30 +0300

    add place_retrive view

--- where_to_go/places/views.py
+++ where_to_go/places/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+from django.shortcuts import  get_object_or_404
 
-# Create your views here.
+from .models import Place
+
+
+def index(request):
+    places = Place.objects.all()
+    data = {"type": "FeatureCollection", "features": []}
+    for place in places:
+        data['features'].append(
+            {
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [place.coordinates['longitude'], place.coordinates['latitude']]
+                },
+                "properties": {
+                    "title": place.title,
+                    "placeId": place.id,
+                    "detailsUrl": "https://raw.githubusercontent.com/devmanorg/where-to-go-frontend/master/places/moscow_legends.json"
+
+                }
+            }
+        )
+
+
+    template = loader.get_template('index.html')
+    context = {'data': data}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
+
+
+def place_retrive(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    print(place)
+    print(place.images)
+    res = {
+        "title": place.title,
+        "imgs": [request.build_absolute_uri(image.image.url) for image in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.coordinates['longitude'],
+            "lat": place.coordinates['latitude']
+        }
+    }
+    return JsonResponse(res, json_dumps_params={'ensure_ascii': False, 'indent': 4})
--- where_to_go/where_to_go/urls.py
+++ where_to_go/where_to_go/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from where_to_go import views
+from places import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('places/<int:place_id>', views.place_retrive)
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
