6472ac5a52418be3d669099b940216039f4ad508
kratorr <31060213+kratorr@users.noreply.github.com>
2020-11-01 19:49:29 +0300

    Update settings.py

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '/templates/static'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'), )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
