2ce5498aa971cb50963c577f26a044b3a5dd53aa
kratorr <smirnovv91@gmail.com>
2020-11-04 17:44:35 +0300

    Add management command load_place

---
+++ where_to_go/places/management/commands/load_place.py
@@ -0,0 +1,34 @@
+import requests
+
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+
+from places.models import Place, Image as PlaceImage
+
+
+class Command(BaseCommand):
+    help = 'Upload place data from json to DB'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', type=str)
+
+    def handle(self, *args, **options):
+        r = requests.get(options['url'])
+        if r.status_code == requests.codes.ok:
+            data = r.json()
+            obj, _ = Place.objects.get_or_create(
+                title=data['title'],
+                description_short=data['description_short'],
+                description_long=data['description_long'],
+                longitude=data['coordinates']['lng'],
+                latitude=data['coordinates']['lat'],
+                defaults={},
+            )
+            for num, url in enumerate(data['imgs']):
+                place_image = PlaceImage.objects.create(place_id=obj.id, number=num)
+                r = requests.get(url)
+                file_name = url.split('/')[-1]
+                if r.status_code == requests.codes.ok:
+                    place_image.image.save(file_name, ContentFile(r.content))
+
+        self.stdout.write(self.style.SUCCESS('Successfully upload place "{}"'.format(obj.title)))
