6559416c9da5739002b509aa4ed4a1b570c5576b
kratorr <smirnovv91@gmail.com>
2020-11-16 21:59:46 +0300

    add raise_for_status() call

--- where_to_go/places/management/commands/load_place.py
+++ where_to_go/places/management/commands/load_place.py
@@ -13,22 +13,22 @@ class Command(BaseCommand):
         parser.add_argument('url', type=str)
 
     def handle(self, *args, **options):
-        r = requests.get(options['url'])
-        if r.status_code == requests.codes.ok:
-            data = r.json()
-            obj, _ = Place.objects.get_or_create(
-                title=data['title'],
-                description_short=data['description_short'],
-                description_long=data['description_long'],
-                longitude=data['coordinates']['lng'],
-                latitude=data['coordinates']['lat'],
-                defaults={},
-            )
-            for num, url in enumerate(data['imgs']):
-                place_image = Image.objects.create(place_id=obj.id, number=num)
-                r = requests.get(url)
-                file_name = url.split('/')[-1]
-                if r.status_code == requests.codes.ok:
-                    place_image.image.save(file_name, ContentFile(r.content))
+        response = requests.get(options['url'])
+        response.raise_for_status()
+        data = response.json()
+        obj, _ = Place.objects.get_or_create(
+            title=data['title'],
+            description_short=data['description_short'],
+            description_long=data['description_long'],
+            longitude=data['coordinates']['lng'],
+            latitude=data['coordinates']['lat'],
+            defaults={},
+        )
+        for num, url in enumerate(data['imgs']):
+            place_image = Image.objects.create(place_id=obj.id, number=num)
+            r = requests.get(url)
+            file_name = url.split('/')[-1]
+            if r.status_code == requests.codes.ok:
+                place_image.image.save(file_name, ContentFile(r.content))
 
         self.stdout.write(self.style.SUCCESS('Successfully upload place "{}"'.format(obj.title)))
